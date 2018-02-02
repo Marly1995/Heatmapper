d8ab018f5bac96d43cab9fb640f429ef3a499c23
Marlon Gilliam <14471455@students.lincoln.ac.uk>
2018-02-02 11:39:38 +0000

    Initial commit

---
+++ Assets/heatmaps/Heatmap.cs
@@ -0,0 +1,78 @@
+﻿// Alan Zucconi
+// www.alanzucconi.com
+using UnityEngine;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Heatmap : MonoBehaviour
+{
+    public string file;
+
+    public Material material;
+
+    public int count;
+
+    TextReader textReader;
+    CsvReader csv;
+
+    List<TransformData> data;
+
+    List<Vector4> positions;
+    public Vector2 parameters;
+
+    private void Start()
+    {
+        data = new List<TransformData>();
+        positions = new List<Vector4>();
+
+        textReader = File.OpenText(file);
+        csv = new CsvReader(textReader);
+        csv.Configuration.RegisterClassMap<TransformDataMap>();
+        IEnumerable<TransformData> enumerable = csv.GetRecords<TransformData>();
+        data = enumerable.ToList();
+        for (int i = 0; i < data.Count; i++)
+        {
+            positions.Add(new Vector4(data[i].X, data[i].Y - 1f, parameters[0], parameters[1]));
+        }
+    }
+
+    void Update()
+    {
+        material.SetInt("_Points_Length", data.Count);
+        material.SetVectorArray("_Points", positions);
+    }
+
+    public class TransformData
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+        public bool Gesturing { get; set; }
+
+        public TransformData() { }
+
+        public TransformData(float x, float y, float z, bool gesture)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            Gesturing = gesture;
+        }
+
+    }
+
+    public sealed class TransformDataMap : ClassMap<TransformData>
+    {
+        public TransformDataMap()
+        {
+            Map(m => m.X).Index(0);
+            Map(m => m.Y).Index(1);
+            Map(m => m.Z).Index(2);
+            Map(m => m.Gesturing).Index(3);
+        }
+    }
+}
